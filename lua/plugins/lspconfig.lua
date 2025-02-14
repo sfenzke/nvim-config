04c92a57e827715f4b5187a7b679f6e5729b0950
Stefan Fenzke <stefan.fezke@gmx.de>
2025-02-14 23:36:40 +0100

    fixed lspo config to not show false warnings

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,3 +1,13 @@
+local lsp_configs = {
+  Lua = {
+      diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = { "vim", "Snacks" },
+      },
+  }
+}
+
+
 return {
 -- Automatically adds lspconfig handers for all language servers installed by mason
   "williamboman/mason-lspconfig.nvim",
@@ -12,7 +22,8 @@ return {
 
     local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-    ---@type MasonLspconfigSettings
+
+    --@type MasonLspconfigSettings
     return {
     ensure_installed = {},
     automatic_installation = false,
@@ -21,7 +32,7 @@ return {
       -- this first function is the "default handler"
       -- it applies to every language server without a "custom handler"
       function(server_name)
-        require("lspconfig")[server_name].setup({ capabilities = capabilities })
+        require("lspconfig")[server_name].setup({ capabilities = capabilities, settings = lsp_configs })
       end,
       },
     }
